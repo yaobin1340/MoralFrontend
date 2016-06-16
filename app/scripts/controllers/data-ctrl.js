8336d4bd183ba20a3863039fc2e865f1e49b7793
yaobin1340 <645894453@qq.com>
2016-06-16 10:03:09 +0800

    111111

--- app/scripts/controllers/data-ctrl.js
+++ app/scripts/controllers/data-ctrl.js
@@ -44,7 +44,9 @@ angular
 				"enabled": false
 			},
 			"loading": false,
-			"size": {},
+			"size": {
+				// width:800
+			},
 
 			yAxis: {
 
@@ -59,12 +61,80 @@ angular
 			},
 
 			func: function (chart) {
-				console.log(chart);
-				$scope.chartData = chart;
-				$scope.chartExport = $.proxy(chart.exportChart, chart);
+				$scope.$evalAsync(function () {
+					chart.reflow();
+				});
 			}
+		}
 
+		$scope.chartConfig2 = {
+			"options": {
+				"chart": {
+					"type": "line"
+				},
+				"plotOptions": {
+					"series": {
+						"stacking": "normal"
+					}
+				}
+			},
+			"series": [
+				{
+					"data": [
+						-10,
+						13,
+						9,
+						7,
+						5,
+						18,
+						9,
+						7,
+						19,
+						12
+					],
+					"id": "series-4"
+				}
+
+			],
+			"title": {
+				"text": "Hello"
+			},
+			"credits": {
+				"enabled": false
+			},
+			"loading": false,
+			"size": {
+				// width:800
+			},
+
+			yAxis: {
+
+				plotLines: [{   //一条延伸到整个绘图区的线，标志着轴中一个特定值。
+					color: 'red',
+					dashStyle: 'LongDashDot', //Dash,Dot,Solid,默认Solid
+					width: 1.5,
+					value: 10,  //y轴显示位置
+					zIndex: 5
+				}]
+
+			},
+
+			func: function (chart) {
+				$scope.chart2 = chart
+				$scope.$evalAsync(function () {
+					chart.reflow();
+				});
+			}
+		}
+
+		$scope.tt = function (){
+			$timeout(function(){
+				$scope.$evalAsync(function () {
+					$scope.chart2.reflow();
+				});
+			},100)
 
 		}
+
 
 	});
